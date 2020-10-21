1678d2cb61cc570b66751f5ab49753e0ea13078a
Washington Barbosa <wassuncaon@gmail.com>
2020-10-21 15:47:09 -0400

    feature: produtos

---
+++ produtos/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Produto
+
+
+class ProdutoSerializer(serializers.ModelSerializer):
+
+    # imagem = serializers.ImageField(max_length=None, use_url=True)
+
+    class Meta:
+        model = Produto
+        fields = (
+            'id',
+            'nome',
+            'preco',
+            'criacao',
+            'atualizacao',
+            'ativo'
+        )
